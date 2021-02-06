00941aa73b8d9630a8097be2c12c776aff59001d
Marcin Szupke <marcin.szupke.sda@gmail.com>
2021-02-06 12:18:13 +0100

    zapis macierzy do pliku

--- src/main/java/io/Macierz.java
+++ src/main/java/io/Macierz.java
@@ -1,5 +1,9 @@
 package io;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 // program, który zapisuje do pliku macierz 10 x 10
 // 1 0 0 0 0 0 0 0 0 0
 // 0 1 0 0 0 0 0 0 0 0
@@ -7,4 +11,29 @@ package io;
 // 0 0 0 1 0 0 0 0 0 0
 // ...
 public class Macierz {
+
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fileWriter = null;
+        fileWriter = new FileWriter("macierz.txt");
+
+        int[][] macierz = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i == j) {
+                    macierz[i][j] = 1;
+                } else {
+                    macierz[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                fileWriter.write(macierz[i][j] + " ");
+            }
+            fileWriter.write("\n");
+        }
+        fileWriter.close();
+    }
 }
